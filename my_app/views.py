1342b5b65af37fd07dd82cb9e57be8389056c823
krishnasankar <sankarkrishna689@gmail.com>
2023-09-04 15:45:45 +0530

    template added

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    # template = loader.get_template("my_app/index.html")
+    return render(request, "my_app/index.html")
