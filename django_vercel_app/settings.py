49568090e8c5d3ccd4863a76fb0769782b9f9eaa
krishnasankar <sankarkrishna689@gmail.com>
2023-09-04 15:28:23 +0530

    settings

--- django_vercel_app/settings.py
+++ django_vercel_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nxs++r5qx*l^=%2tkk2rpov_6l^*nzz87#v-i+*a$6311()ztz'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
